ffcba255916c09f56de040b01c2a5c6c5a86f9fc
vitor paiva <paivavitorr@gmail.com>
2024-03-18 15:32:01 -0400

    thirds

--- projects/projetos_repor/tela-login/login-v2/src/App.js
+++ projects/projetos_repor/tela-login/login-v2/src/App.js
@@ -1,36 +1,48 @@
+import { useState } from "react";
+import "./styles.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div className = "container">
+    <div className="container">
       <div className="container-login">
         <div className="wrap-login">
           <form className="login-form">
+            <span className="login-form-title"> Bem vindo </span>
+            <div className="wrap-input">
+              <input
+                className={email !== "" ? "has-val input" : "input"}
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+            <div className="wrap-input">
+              <input
+                className={password !== "" ? "has-val input" : "input"}
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
 
-           <span className="login-form-title">Bem Vindo!</span>
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Login</button>
+            </div>
 
-          <div className="wrap-imput">
-            <input type="email"/>
-            <span className="focus-input" data-placeholder='Email'></span> 
-          </div>
-
-          <div className="wrap-imput">
-            <input type="password"/>
-            <span className="focus-input" data-placeholder="Password"></span> 
-          </div>
-
-          <div className="container-login-form-btn">
-            <button className="login-form-btn">Login</button>
-          </div>
-
-          <div className="text-center">Não possui conta?</div>
-
-          <a className="txt2" href="#">Criar conta.</a>
-
+            <div className="text-center">
+              <span className="txt1">Não possui conta? </span>
+              <a className="txt2" href="#">
+                Criar conta
+              </a>
+            </div>
           </form>
         </div>
-      </div> 
-    </div> 
+      </div>
+    </div>
   );
 }
-
 export default App;
